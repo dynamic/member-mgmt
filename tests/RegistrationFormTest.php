3e34089dc70f56739ae40fb6edd1761101c307bc
Jason Irish <jirish@dynamicagency.com>
2017-06-14 19:35:06 -0500

    bugfix - Member declared twice in config.yml
    DynamicMemberDataExtension was not being applied

---
+++ tests/RegistrationFormTest.php
@@ -0,0 +1,9 @@
+<?php
+
+class RegistrationFormTest extends SapphireTest
+{
+    public function test__construct()
+    {
+        $this->markTestSkipped('TODO');
+    }
+}
---
+++ tests/Registration_ControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+class Registration_ControllerTest extends SapphireTest
+{
+    public function testIndex()
+    {
+        $this->markTestSkipped('TODO');
+    }
+
+    public function testRegistrationForm()
+    {
+        $this->markTestSkipped('TODO');
+    }
+
+    public function testProcessmember()
+    {
+        $this->markTestSkipped('TODO');
+    }
+}
